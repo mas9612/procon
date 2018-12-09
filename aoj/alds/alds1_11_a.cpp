513b94a8be161726dcd3b3ae7cf43689b11f89ba
mas9612 <mas9612@gmail.com>
2018-12-09 17:44:27 +0900

    change to 0 origin graph

--- aoj/alds/alds1_11_a.cpp
+++ aoj/alds/alds1_11_a.cpp
@@ -8,20 +8,22 @@ int main()
     int n;
     cin >> n;
 
-    vector<vector<int>> G(n+1, vector<int>(n+1, 0));
+    vector<vector<int>> G(n, vector<int>(n, 0));
     for (int i = 0; i < n; ++i) {
         int v, nv;
         cin >> v >> nv;
+        --v;
         for (int j = 0; j < nv; ++j) {
             int adjacency;
             cin >> adjacency;
+            --adjacency;
             G[v][adjacency] = 1;
         }
     }
 
-    for (int i = 1; i <= n; ++i) {
-        cout << G[i][1];
-        for (int j = 2; j <= n; ++j)
+    for (int i = 0; i < n; ++i) {
+        cout << G[i][0];
+        for (int j = 1; j < n; ++j)
             cout << ' ' << G[i][j];
         cout << '\n';
     }
