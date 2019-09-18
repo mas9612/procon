0f40553024caab186cf74b59ccc6001d56cbb271
Masato Yamazaki <mas9612@gmail.com>
2019-09-18 22:57:07 +0900

    re-solve alds1_3_a

--- aoj/alds/alds1_3_a_without_stl.cpp
+++ aoj/alds/alds1_3_a_without_stl.cpp
@@ -5,8 +5,22 @@
 
 using namespace std;
 
+class Stack {
+    int array[100];
+    int top_idx;
+
+   public:
+    Stack() { top_idx = -1; }
+    int top() { return array[top_idx]; }
+    void push(int n) {
+        ++top_idx;
+        array[top_idx] = n;
+    }
+    void pop() { --top_idx; }
+};
+
 int main() {
-    stack<int> stack;
+    Stack stack;
 
     string s;
     getline(cin, s);
