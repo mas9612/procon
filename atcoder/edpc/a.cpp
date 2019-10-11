1720e11cb45d7a173487201d9813572af29021b7
Masato Yamazaki <mas9612@gmail.com>
2019-10-11 22:34:43 +0900

    re-solve edpc a

--- atcoder/edpc/a.cpp
+++ atcoder/edpc/a.cpp
@@ -1,26 +1,10 @@
+#include <algorithm>
+#include <climits>
 #include <cmath>
 #include <iostream>
-#include <utility>
 #include <vector>
 
 using namespace std;
-
-int min_cost(vector<int>& costs) {
-    const int INF = 10e5;
-    int size = costs.size();
-    vector<int> calc(size, INF);
-
-    calc[0] = 0;
-    for (int i = 1; i < size; ++i) {
-        int cost_a = INF, cost_b = INF;
-        if (i - 1 >= 0)
-            cost_a = calc[i - 1] + abs(costs[i] - costs[i - 1]);
-        if (i - 2 >= 0)
-            cost_b = calc[i - 2] + abs(costs[i] - costs[i - 2]);
-        calc[i] = min(cost_a, cost_b);
-    }
-    return calc[size - 1];
-}
 
 int main() {
     int n;
@@ -30,5 +14,14 @@ int main() {
     for (int i = 0; i < n; ++i)
         cin >> h[i];
 
-    cout << min_cost(h) << '\n';
+    vector<int> dp(n, INT_MAX);
+    dp[0] = 0;
+    dp[1] = abs(h[1] - h[0]);
+    for (int i = 2; i < n; ++i) {
+        int cost1 = dp[i - 1] + abs(h[i] - h[i - 1]);
+        int cost2 = dp[i - 2] + abs(h[i] - h[i - 2]);
+        dp[i] = min(cost1, cost2);
+    }
+    cout << dp[n - 1] << '\n';
 }
+
