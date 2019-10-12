dd11ad35adf89f7bd3965dbcaba210dab60f8d6b
Masato Yamazaki <mas9612@gmail.com>
2019-10-12 17:19:43 +0900

    re-solve edpc b

--- atcoder/edpc/b.cpp
+++ atcoder/edpc/b.cpp
@@ -1,35 +1,29 @@
 #include <climits>
+#include <cmath>
 #include <iostream>
-#include <utility>
 #include <vector>
 
 using namespace std;
-
-int min_cost(vector<int>& costs, int k) {
-    const int INF = INT_MAX;
-    int size = costs.size();
-    vector<int> calc(size, INF);
-
-    calc[0] = 0;
-    for (int i = 1; i < size; ++i) {
-        for (int j = 1; j <= k; ++j) {
-            if (i - j < 0)
-                break;
-            int c = calc[i - j] + abs(costs[i] - costs[i - j]);
-            if (calc[i] > c)
-                calc[i] = c;
-        }
-    }
-    return calc[size - 1];
-}
 
 int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<int> costs(n);
+    vector<int> h(n);
     for (int i = 0; i < n; ++i)
-        cin >> costs[i];
+        cin >> h[i];
 
-    cout << min_cost(costs, k) << '\n';
+    vector<int> dp(n, INT_MAX);
+    dp[0] = 0;
+    for (int i = 1; i < n; ++i) {
+        for (int j = 1; j <= k; ++j) {
+            if (i - j < 0)
+                break;
+
+            dp[i] = min(dp[i], dp[i - j] + abs(h[i] - h[i - j]));
+        }
+    }
+
+    cout << dp[n - 1] << '\n';
 }
+
