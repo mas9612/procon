f54b3101934f210177977625b7d1f9b0160aea89
Masato Yamazaki <mas9612@gmail.com>
2019-10-14 17:26:02 +0900

    re-solve edpc d

--- atcoder/edpc/d.cpp
+++ atcoder/edpc/d.cpp
@@ -1,31 +1,31 @@
-#include <iomanip>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 using namespace std;
-
-struct item {
-    int weight;
-    int value;
-};
 
 int main() {
     int n, w;
     cin >> n >> w;
 
-    vector<item> items(n + 1, {0, 0});
-    for (int i = 1; i <= n; ++i)
-        cin >> items[i].weight >> items[i].value;
+    vector<pair<long long, long long>> goods(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        int w, v;
+        cin >> w >> v;
+        goods[i] = make_pair(w, v);
+    }
 
-    vector<vector<long>> dp(n + 1, vector<long>(w + 1, 0));
+    vector<vector<long long>> dp(n + 1, vector<long long>(w + 1, 0));
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= w; ++j) {
             dp[i][j] = dp[i - 1][j];
-            if (j - items[i].weight >= 0) {
+            if (j >= goods[i].first) {
                 dp[i][j] = max(dp[i][j],
-                               dp[i - 1][j - items[i].weight] + items[i].value);
+                               dp[i - 1][j - goods[i].first] + goods[i].second);
             }
         }
     }
+
     cout << dp[n][w] << '\n';
 }
+
